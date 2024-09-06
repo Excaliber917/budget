9704c4b797108d930ea69a9b11bbed3a673aad95
Dhruba <93818182+Excaliber917@users.noreply.github.com>
2024-09-06 16:05:05 +0530

    token genration fixed

--- server/index.js
+++ server/index.js
@@ -11,9 +11,8 @@ dotenv.config()
 const app = express()
 
 const corsOptions = {
-    origin: 'https://localhost:5173', //  frontend URL
-    // origin: 'https://fintech-8xnk.onrender.com', //  frontend URL
-    optionsSuccessStatus: 200,
+    // origin: 'https://localhost:5173', //  frontend URL
+    origin: 'https://fintech-8xnk.onrender.com', //  frontend URL
     credentials: true,
 };
 app.use(cors(corsOptions))
